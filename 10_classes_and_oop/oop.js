b7e7bdbb23df6d4b98b3c3e172d4ef529fbebbe5
Vikaskumain <121432906+Vikaskumain@users.noreply.github.com>
2024-04-11 18:46:07 +0530

    first commit

---
+++ 10_classes_and_oop/oop.js
@@ -0,0 +1,52 @@
+// const user = {
+//     username: "hitesh",
+//     loginCount: 8,
+//     signedIn: true,
+
+//     getUserDetails: function(){
+//         //console.log("Got user details from database");
+//         // console.log(`Username: ${this.username}`);
+//         console.log(this);
+//     }
+
+// }
+
+// //console.log(user.username)
+// //console.log(user.getUserDetails());
+// // console.log(this);
+
+
+// function User(username, loginCount, isLoggedIn){
+//     this.username = username;
+//     this.loginCount = loginCount;
+//     this.isLoggedIn = isLoggedIn
+
+//     this.greeting = function(){
+//         console.log(`Welcome ${this.username}`);
+
+//     }
+
+//     return this
+// }
+
+// const userOne = new User("hitesh", 12, true)
+// const userTwo = new User("ChaiAurCode", 11, false)
+// console.log(userOne.constructor);
+//console.log(userTwo);
+
+
+
+// rivison
+ const vikas = {
+    firstName:"Vikas",
+    lastName:"kumain",
+    age:52,
+    getUserName:function(){
+console.log(`username:${ this.firstName} ${this.lastName} ${this.age} yes`) 
+    }
+   
+ }
+
+// console.log(vikas.getUserName())
+console.log(vikas)
+c
